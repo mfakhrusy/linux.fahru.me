d0c898b5d840dd768cb07282b6e22a8c53c912b8
Fakhrusy Muhamad <hello@fahru.me>
2026-06-08 21:33:35 -0700

    update

--- src/components/desktop/apps/AppAboutSite.tsx
+++ src/components/desktop/apps/AppAboutSite.tsx
@@ -11,7 +11,7 @@ This is Fahru personal site
 
 Available on github:
 
-[https://github.com/mfakhrusy/fahru.me](https://github.com/mfakhrusy/fahru.me) <ExternalLinkIcon />
+[https://github.com/mfakhrusy/linux.fahru.me](https://github.com/mfakhrusy/linux.fahru.me) <ExternalLinkIcon />
 
 <br />
 
@@ -19,8 +19,8 @@ Available on github:
 
 - typescript
 - reactJS
-- nextJS
-- vercel
+- <s>nextJS</s> <a href="https://github.com/cloudflare/vinext" target="_blank" rel="noopener noreferrer">vinext</a> <ExternalLinkIcon />
+- <s>vercel</s> Cloudflare :)
 - xtermjs
 - chakra-ui
 - framer-motion
@@ -29,7 +29,7 @@ Available on github:
 
 ## **Icons**
 
-[Moka Icons](https://snwh.org/moka) by [Sam Hewitt](https://samuelhewitt.com) 
+[Moka Icons](https://snwh.org/moka) by [Sam Hewitt](https://samuelhewitt.com) | Update 2026: The project had been discontinued!
 
 licensed under [CC-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) <ExternalLinkIcon />
 
@@ -37,7 +37,7 @@ licensed under [CC-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) <Ext
 
 ## **Background**
 
-[GitHub](https://github.com/PineAndApplePizza/open-wallpapers/) by PineAndApplePizza
+<s>[GitHub](https://github.com/PineAndApplePizza/open-wallpapers/) by PineAndApplePizza</s> it's gone (≧︿≦)
 
 licensed under [GPL](https://www.gnu.org/licenses/gpl-3.0.html)
 
@@ -45,47 +45,7 @@ original logo creator [u/Ishaan_P](https://www.reddit.com/user/Ishaan_P)
 
 <br />
 
-## **About site**
-
-This is Fahru personal site
-
-<br />
-
-## **Source Code**
-
-Available on github:
-
-[https://github.com/mfakhrusy/fahru.me](https://github.com/mfakhrusy/fahru.me) <ExternalLinkIcon />
-
-<br />
-
-## **Tech Stack & Libraries**
-
-- typescript
-- reactJS
-- nextJS
-- vercel
-- xtermjs
-- chakra-ui
-- framer-motion
-
-<br />
-
-## **Icons**
-
-[Moka Icons](https://snwh.org/moka) by [Sam Hewitt](https://samuelhewitt.com) 
-
-licensed under [CC-SA-4.0](https://creativecommons.org/licenses/by-sa/4.0/) <ExternalLinkIcon />
-
-<br />
-
-## **Background**
-
-[GitHub](https://github.com/PineAndApplePizza/open-wallpapers/) by PineAndApplePizza
-
-licensed under [GPL](https://www.gnu.org/licenses/gpl-3.0.html)
-
-original logo creator [u/Ishaan_P](https://www.reddit.com/user/Ishaan_P)  
+<br /> 
 `;
 
 export function AppAboutSite() {
