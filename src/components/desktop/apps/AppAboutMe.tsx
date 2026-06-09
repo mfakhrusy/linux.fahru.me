c5253b9bf0234a37714e10adc53d0ab0ab2ac01a
Fakhrusy Muhamad <hello@fahru.me>
2026-06-08 23:40:15 -0700

    Update about me

--- src/components/desktop/apps/AppAboutMe.tsx
+++ src/components/desktop/apps/AppAboutMe.tsx
@@ -3,55 +3,33 @@ import { AppMarkdownLayout } from "@/components/desktop/apps/layout/AppMarkdownL
 const md = `
 ## **About me**
 Fahru, Tucson, AZ, USA  
-Origin: Indonesia  
+Origin: Indonesia
 
 <br />
 
-## **What I do**
-Software Engineer. Tinkerer. Hacker.  
+## **Professional Headline**
+Engineer. Researcher.
 
 <br />
 
-## **Tech stacks I worked on**
-- Web stacks: HTML5, CSS3, Javascript
-- Typescript
-- ReasonML
-- React
-- NodeJS
-- PostgreSQL
+## **What I am**
+- A muslim
+- A dad of 8 year old
+- A husband
+- I may or may not have an ADHD, but my kid (probably) has it
 
 <br />
 
-## **Other skills**
-- CI/CD with Gitlab CI, and GitHub Action
-- Linux web server (nginx/apache) config
-- DNS config
-- Web animation & SVGs
-- Docker
+## **What I am not**
+- Techbro; I loathe the idea of modern techbro. The philosophical type. The one with "feel the AGI"-esque personality or whatever BS they spew. "Tech shouldn't be depressing" is a twt that I saw once in twitter and I can relate to the notion because right now (2026), tech feels bleak; it was not as fun and positive as it was in 2022.
+- AI maximalist. Specifically, I don't think the current LLM will give birth to conscious/sentient robot. They're tools. Clankers. Whatever. It does the job, or it doesn't. They may be correct, or they may not. They're literally just math. AI has its uses, but getting into the realm of consciousness from LLM is just wasting time.
 
 <br />
 
-## **Currently interested about:**
-- Web Assembly with either Rust/C++
-- Building graphic-intensive web application
-- Cyber Security (especially CTF)
-- etc... follow me on [mastodon](https://fosstodon.org/fahru) to find out!
-
-<br />
-
-## **My take on software engineering**
-- Problem solving at its core
-- Maintainable and readable code is important
-- Pragmatism on tight deadline
-- Programming language is just a tool
-
-<br />
-
-**Hobbies**
-- Learning stuffs. Anything, really.
-- Writing. I'm preparing a blog!
-- Building something cool. Tinkering about stuffs.
-- Reading articles on the web & cool stuffs on twitter, still working on developing reading books as a habit.
+## **What I like to do (or what I think I like to do...)**
+- Write poems (in both my native language -- Indonesian -- or English)
+- Read book (I like fiction books, I hate self-help books).
+- I collect textbooks for the love of the game (they're nice to look at).
 
 `;
 
