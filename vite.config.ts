6d98a4e9c938a8e2b19f4240dd335ab595f0455a
Fakhrusy Muhamad <hello@fahru.me>
2026-06-08 18:37:39 -0700

    fix npm run dev script

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import vinext from "vinext";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [vinext(), cloudflare()],
+export default defineConfig(({ command }) => ({
+  plugins: [vinext(), command === "build" && cloudflare()],
   ssr: {
     noExternal: [/^@emotion\//, "@chakra-ui/react"],
   },
-});
+}));
